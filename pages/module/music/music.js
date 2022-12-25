c3e8be1f383cf47b18659dc1707d6880377453d9
shenjy <shenjy24@gmail.com>
2022-12-25 14:09:32 +0800

    播发器

--- pages/module/music/music.js
+++ pages/module/music/music.js
@@ -1,12 +1,56 @@
+const { getComponentCountFromAttributeName } = require("WechatXrFrame/loader/glTF/geometry/primitives/attributes/GLTFAttributesNode")
+
 // pages/module/music/music.js
 Page({
+
+    audioCtx: null,
 
     /**
      * 页面的初始数据
      */
     data: {
         item: 0,
-        tab: 0
+        tab: 0,
+        playlist: [
+            {
+                id: 1,
+                title: '钢琴协奏曲',
+                singer: '肖邦',
+                src: '/videos/起风了.mp3',
+                coverImgUrl: '/images/cover.png'
+            },
+            {
+                id: 2,
+                title: '奏鸣曲',
+                singer: '莫扎特',
+                src: '/videos/起风了.mp3',
+                coverImgUrl: '/images/cover.png'
+            },
+            {
+                id: 3,
+                title: '欢乐颂',
+                singer: '贝多芬',
+                src: '/videos/起风了.mp3',
+                coverImgUrl: '/images/cover.png'
+            },
+            {
+                id: 4,
+                title: '爱之梦',
+                singer: '李斯特',
+                src: '/videos/起风了.mp3',
+                coverImgUrl: '/images/cover.png'
+            }
+        ],
+        state: 'paused',
+        playIndex: 0,
+        play: {
+            currentTime: '00:00',
+            duration: '00:00',
+            percent: 0,
+            title: '',
+            singer: '',
+            coverImgUrl: '/images/cover.png'
+        },
     },
 
     changeItem(e) {
@@ -15,6 +59,38 @@ Page({
 
     changeTab(e) {
         this.setData({ tab: e.detail.current })
+    },
+
+    setMusic(index) {
+        let music = this.data.playlist[index]
+        this.audioCtx.src = music.src
+        this.setData({
+            playIndex: index,
+            'play.title': music.title,
+            'play.singer': music.singer,
+            'play.coverImgUrl': music.coverImgUrl,
+            'play.currentTime': '00:00',
+            'play.duration': '00:00',
+            'play.percent': 0
+        })
+    },
+
+    play() {
+        this.audioCtx.play()
+        this.setData({ state: 'running' })
+    },
+
+    pause() {
+        this.audioCtx.pause()
+        this.setData({ state: 'paused' })
+    },
+
+    next() {
+        let index = this.data.playIndex >= this.data.playlist.length - 1 ? 0 : this.data.playIndex + 1
+        this.setMusic(index)
+        if (this.data.state === 'running') {
+            this.play()
+        }
     },
 
     /**
@@ -28,7 +104,38 @@ Page({
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady() {
+        this.audioCtx = wx.createInnerAudioContext()
+        this.audioCtx.onError(() => {
+            console.log('播放失败：' + this.audioCtx.src)
+        })
+        // 播发完自动换下一首
+        this.audioCtx.onEnded(() => {
+            this.next()
+        })
+        // 自动更新播放进度
+        this.audioCtx.onPlay(() => {
 
+        })
+        this.audioCtx.onTimeUpdate(() => {
+            this.setData({
+                'play.duration': this.formatTime(this.audioCtx.duration),
+                'play.currentTime': this.formatTime(this.audioCtx.currentTime),
+                'play.percent': this.audioCtx.currentTime / this.audioCtx.duration * 100
+            })
+        })
+        //默认选择第1曲
+        this.setMusic(0)
+    },
+
+    formatTime(time) {
+        let minute = Math.floor(time / 60) % 60
+        let second = Math.floor(time) % 60
+        return (minute < 10 ? '0' + minute : minute) + ":" + (second < 10 ? '0' + second : second)
+    },
+
+    sliderChange(e) {
+        let second = e.detail.value * this.audioCtx.duration / 100
+        this.audioCtx.seek(second)
     },
 
     /**
